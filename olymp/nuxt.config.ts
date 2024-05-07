f9e266760ec2d88db069d65ebbbc7c3bc9e8749d
Олег Площинець <oleg.pl2006@gmail.com>
2024-05-07 11:50:04 +0300

    Виправив баг підключенням стилів

--- olymp/nuxt.config.ts
+++ olymp/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/supabase','@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      link: [
+        {
+          href: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600;700&display=swap",
+          rel: 'stylesheet'
+        },
+        {
+          href: "https://fonts.gstatic.com",
+          rel: "preconnect"
+        },
+        {
+          href: "https://fonts.googleapis.com",
+          rel: "preconnect"
+        }
+      ]
+    }
+  },
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
   devtools: { enabled: true }
 })
